0fa67af0c89045c2066a98a0a2d4fc3f65bb30f9
Mauricio <mauriciobastiani@outlook.com>
2020-09-09 15:13:39 -0300

    Removing unused modules

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
 import { AppCartoesService } from './app.cartoes.service';
 import { CartoesComponent } from "./cartoes/cartoes.component";
@@ -18,8 +16,6 @@ import { CartoesComponent } from "./cartoes/cartoes.component";
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     CartoesComponent
   ],
   imports: [
@@ -29,8 +25,6 @@ import { CartoesComponent } from "./cartoes/cartoes.component";
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'cartoes', component: CartoesComponent }
     ])
   ],
